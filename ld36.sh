2e403e17e49ae72227708f2b5e5d95c0e640ab81
Skial Bainn <skial.bainn@gmail.com>
2016-09-16 13:51:16 +0100

    Rewrite screengrab subscript.

--- ld36.sh
+++ ld36.sh
@@ -25,9 +25,9 @@ mkdir -p $OPT_DIR
 #ldscraper.sh 36
 
 electron --enable-logging ./render/index.js \
--md markdown-it-abbr markdown-it-attrs markdown-it-emoji markdown-it-footnote \
-markdown-it-headinganchor \
+-md markdown-it-abbr markdown-it-attrs markdown-it-emoji \
+markdown-it-footnote markdown-it-headinganchor \
 -s ./render/build.js -r src -i $INPUT -o $BIN_DIR$BIN_BASE/index.html -b src \
 -j ./src/data/ld36.json ./src/data/ld36.manual.json \
---scripts ../site/subresourceintegrity.js ../site/font.characters.js \
--rs bin --show
+--scripts ../site/subresourceintegrity.js ../site/font.characters.js ../site/screengrab.js \
+-rs bin -w 1920 -h 1080 --show
