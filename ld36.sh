e2e3f46d38646d00d6d8f13ee8d22a1f14229461
Skial Bainn <skial.bainn@gmail.com>
2016-09-19 11:59:25 +0100

    Update paths.

--- ld36.sh
+++ ld36.sh
@@ -14,13 +14,13 @@ MIN_BASE="${MIN_BASE/.md/}"
 OPT=./opt$STR
 OPT_DIR=${OPT%/*}
 OPT_BASE="${OPT/$OPT_DIR/}"
-OPT_BASE="${OPT_BASE/.html/}"
+OPT_BASE="${OPT_BASE/.md/}"
 OPT_CRIT=$OPT_DIR$OPT_BASE.crt.html
 OPT_TWIN=$OPT_DIR$OPT_BASE.opt.html
 
-mkdir -p $BIN_DIR
-mkdir -p $MIN_DIR
-mkdir -p $OPT_DIR
+mkdir -p $BIN_DIR$BIN_BASE
+mkdir -p $MIN_DIR$MIN_BASE
+mkdir -p $OPT_DIR$OPT_BASE
 
 #ldscraper.sh 36
 
@@ -30,9 +30,9 @@ markdown-it-footnote markdown-it-headinganchor \
 -s ./render/build.js -r src -i $INPUT -o $BIN_DIR$BIN_BASE/index.max.html -b src \
 -j ./src/data/ld36.json ./src/data/ld36.manual.json \
 --scripts ../site/subresourceintegrity.js ../site/font.characters.js ../site/screengrab.js \
--rs bin -w 1920 -h 1080
+-rs opt -w 1920 -h 1080
 
 html-minifier \
 --collapse-boolean-attributes --remove-comments --remove-empty-attributes --remove-redundant-attributes \
 --collapse-whitespace --decode-entities --remove-style-link-type-attributes \
---remove-script-type-attributes -o $BIN_DIR$BIN_BASE/index.html $BIN_DIR$BIN_BASE/index.max.html
+--remove-script-type-attributes -o $OPT_DIR$OPT_BASE/index.html $BIN_DIR$BIN_BASE/index.max.html
