d98b3f3c139efe8054d49c987999f03e1358a25a
skial <skial@skial-ms7d78>
2025-12-16 10:33:05 +0000

    Remove all-css.sh, all-extras.sh, all-html.sh, and 56 more files

--- all-css.sh
+++
@@ -1,2 +0,0 @@
-INPUT="$@"
-find $INPUT -name "*.css" | xargs -P 7 -n 1 cssmin.sh
--- patreonscraper.sh
+++
@@ -1,1 +0,0 @@
-electron --enable-logging ./patreon/index.js -s ./patreon/scraper.js -o ./src/data/patreons.json -i ./src/data/patreons.original.json
